775d32c3ef8f32068430d425cbf1f6a00e766376
RomanVelky <roman.velky@proton.me>
2024-10-24 11:34:29 +0200

    feat: card-detail layout + validate zod schema

--- src/app/car-detail/car-detail.component.ts
+++ src/app/car-detail/car-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ReactiveFormsModule,
@@ -12,6 +12,8 @@ import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { Car, CarBrand } from '../models/car.model';
+import { z } from 'zod';
+import { CarService } from '../services/car.service';
 
 @Component({
   selector: 'app-car-detail',
@@ -28,14 +30,16 @@ import { Car, CarBrand } from '../models/car.model';
   templateUrl: './car-detail.component.html',
   styleUrls: ['./car-detail.component.scss'],
 })
-export class CarDetailComponent {
-  @Input() car: Car | null;
-  carForm: FormGroup;
-  carBrands: CarBrand[];
+export class CarDetailComponent implements OnInit {
+  @Input() car!: Car;
+  @Output() closeEmitter = new EventEmitter<Event>();
 
-  constructor(private fb: FormBuilder) {
-    this.car = null;
-    this.carBrands = ['AUDI', 'BMW', 'MERCEDES', 'SKODA'];
+  carForm: FormGroup;
+
+  carBrands: CarBrand[] = ['AUDI', 'BMW', 'MERCEDES', 'SKODA'];
+  carFormSchema = z.object({});
+
+  constructor(private fb: FormBuilder, private carService: CarService) {
     this.carForm = this.fb.group({
       id: [{ value: '', disabled: true }, Validators.required],
       licencePlate: ['', Validators.required],
@@ -45,19 +49,39 @@ export class CarDetailComponent {
     });
   }
 
-  ngOnChanges() {
-    if (this.car) {
-      this.carForm.patchValue(this.car);
-    }
+  ngOnInit(): void {
+    this.carService.getCarss().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error fetching cars', error);
+      },
+    });
+
+    const carBrands = ['AUDI', 'BMW', 'MERCEDES', 'SKODA'] as const;
+
+    this.carFormSchema = z.object({
+      licencePlate: z.string().min(1, 'Licence plate cannot be empty'),
+      brand: z.enum(carBrands, { required_error: 'Brand is required' }),
+      price: z.number().min(100, 'Price must be greater than 100'),
+      createdDate: z.date(),
+    });
   }
 
   saveCar() {
-    if (this.carForm.valid) {
+    const result = this.carFormSchema.safeParse(this.carForm.value);
+
+    if (result.success) {
       // Save the car details logic
+      console.log('Car details are valid:', result.data);
+    } else {
+      // Handle validation errors
+      console.log('Validation errors:', result.error.format());
     }
   }
 
-  close() {
-    // Logic to close the sidebar
+  emitClose(event: Event) {
+    this.closeEmitter.emit(event);
   }
 }
